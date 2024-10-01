410639415dfa332864577b9249f1fed764b979d4
Mateus de Barros <97922225+MateusDBarros@users.noreply.github.com>
2024-10-01 02:49:18 -0300

    Update sort and search.c

--- sort and search.c
+++ sort and search.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int binary(int array[],int left,int right, int target);
+//Cria as funçoes
+int binary(int array[],int esquerda,int direita, int elemento);
 void bubble(int array[], int size);
 
 int main(void)
 {
-  //Create an array with unsorted numbers
-  int list[] = {  36,   62,   86,    105,  103,
+  //Cria uma array de numeros desordenados
+  int lista[] = {  36,   62,   86,    105,  103,
                   123,  113,  21,    134,  50,
                   133,  28,   78,    56,   79,
                   33,   108,  22,    4,    73,
@@ -18,33 +19,34 @@ int main(void)
                   120,  101,  122,   65,   85,
                   58,   48,   129,   38,   63};
 
-  //Get the total elements of the array
-  int len = sizeof(list)/sizeof(list[0]);
+  //Obtem o total de elementos da array
+  int len = sizeof(lista)/sizeof(lista[0]);
   
-  //Call the sort function
-  bubble(list, len);
+  //Chama a função
+  bubble(lista, len);
 
-  //assign
-  int index = binary(list, 0, len - 1, 65);
+  //Atribui
+  int index = binary(lista, 0, len - 1, 65);
    
-
-  printf("The index of the number %d is at position: %d\n", list[index], index);
+  //Printa o resultado
+  printf("The index of the number %d is at position: %d\n", lista[index], index);
 
 }
-int binary(int array[], int left, int right, int target)
+//Função de busca binaria (Binary Search)
+int binary(int array[], int esquerda, int direita, int elemento)
 {
-  int mid = left + (right - left) / 2;
+  int meio = esquerda + (direita - esquerda) / 2;
 
-  if(target == array[mid]) return mid;
+  if(elemento == array[meio]) return meio;
+  if (esquerda > direita) return 1;
 
-  if (left > right) return 1;
-
-  if (array[mid] < target) binary(array, mid + 1, right, target);
+  if (array[meio] < elemento) binary(array, meio + 1, direita, elemento);
   
-  else binary(array, left, mid - 1, target);
+  else binary(array, esquerda, meio - 1, elemento);
 
 }
 
+//Função de ordenação (Bubble Sort)
 void bubble(int array[], int size)
 {
   for (int i = 0; i < size - 1; i++)
